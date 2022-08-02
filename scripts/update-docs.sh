64440acf233a75c86e2cff20791c47eeef78e6fc
elf-mouse <ifmouz@gmail.com>
2022-08-02 17:10:56 +0800

    chore: update

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -8,4 +8,4 @@ cd dist
 git init
 git add -A
 git commit -m "update official site for balm-ui-pro@$LegacyVersion"
-git push -f git@github.com:balmjs/legacy.pro.balmjs.com.git main:gh-pages
+git push -f git@github.com:balmjs/legacy.pro.balmjs.com.git master:gh-pages
