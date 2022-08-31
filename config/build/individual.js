a12e5f4fdaaa763ad85dd6125f13dbe80e33d958
elf-mouse <ifmouz@gmail.com>
2022-08-31 15:47:50 +0800

    chore: update

--- config/build/individual.js
+++ config/build/individual.js
@@ -1,6 +1,90 @@
+const fs = require('fs');
 const config = require('./config');
 
+const individualBuild = ['components', 'plugins'];
+
+function getComponentName(item) {
+  return item
+    .split('-')
+    .map((name) => name.replace(/^\S/, (s) => s.toUpperCase()))
+    .join('');
+}
+
+function getLibrary(buildName, item) {
+  let library;
+
+  switch (buildName) {
+    case 'components':
+      library = `Ui${getComponentName(item)}`;
+      break;
+    case 'plugins':
+      library = `$${item}`;
+      break;
+    default:
+      library = item;
+  }
+
+  console.log(`library: ${library}`);
+
+  return library;
+}
+
 function buildIndividual(mix) {
+  console.log('Individual libraries:');
+
+  // Clear individual
+  mix.remove([config.output.components, config.output.plugins]);
+
+  const uiOutput = `${config.output.dist}/css/balm-ui`;
+  individualBuild.forEach((buildName) => {
+    config[buildName].forEach((item) => {
+      const libraryName = getLibrary(buildName, item);
+
+      let jsInput;
+      switch (buildName) {
+        case 'components':
+          jsInput = {
+            index: `${config.input[buildName]}/${item}/index.js`
+          };
+          break;
+        default:
+          jsInput = {
+            index: `${config.input[buildName]}/${item}.js`
+          };
+      }
+      const jsOutput = `${uiOutput}/${buildName}/${item}`;
+
+      mix.webpack(jsInput, jsOutput, {
+        output:
+          buildName === 'plugins'
+            ? {
+                library: {
+                  name: libraryName,
+                  type: 'umd',
+                  umdNamedDefine: true
+                }
+              }
+            : {
+                library: {
+                  name: libraryName,
+                  type: 'umd',
+                  export: 'default',
+                  umdNamedDefine: true
+                }
+              }
+      });
+
+      const sassFolder = `${config.input.sass}/balm-ui/${buildName}`;
+      if (fs.existsSync(sassFolder)) {
+        mix.copy(`${sassFolder}/**/*`, `${uiOutput}/${buildName}`);
+      }
+    });
+  });
+
+  individualBuild.forEach((buildName) => {
+    mix.copy(`${uiOutput}/${buildName}/**/*`, buildName);
+  });
+
   mix.copy(['./dist/css/*.css', './dist/js/*.js'], config.output.dist);
   mix.remove(['./dist/css', './dist/js']);
 }
