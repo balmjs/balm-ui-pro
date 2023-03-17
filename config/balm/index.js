0de6aa6cc4bf11bfb2670b4e951cea7dcf740128
elf-mouse <ifmouz@gmail.com>
2023-03-17 13:02:29 +0800

    chore: clean code

--- config/balm/index.js
+++ config/balm/index.js
@@ -134,7 +134,7 @@ function getConfig(balm) {
       options: {
         globIgnores: ['404.html']
       },
-      version: `v${pkg.version.replace(/\./g, '')}-0317`
+      version: `v${pkg.version.replace(/\./g, '')}`
     },
     logs: {
       level: 2
