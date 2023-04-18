2248ab6f80f60304e2d869bb6f0c1ef373ce7c05
elf-mouse <ifmouz@gmail.com>
2023-04-18 14:43:12 +0800

    chore: clean code

--- docs/scripts/components/views/list-view.js
+++ docs/scripts/components/views/list-view.js
@@ -17,8 +17,8 @@ export async function getModelListDataFn({ model, searchForm, listData }) {
   return await store.getModelList(model, params);
 }
 
-export async function handleRowAction(action, data, listViewData) {
-  isDev && console.log('handleRowAction', action, data, listViewData);
+export async function handleRowAction(action, rowData, listViewData) {
+  isDev && console.log('handleRowAction', action, rowData, listViewData);
 }
 
 export async function handleTopAction(action, listViewData) {
