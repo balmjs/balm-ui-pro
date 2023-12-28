9933a0e0ad1634f4af143bb2f1ed3cf7bb9046d7
elf-mouse <ifmouz@gmail.com>
2023-12-28 12:06:03 +0800

    build: docs-vue3 from main as of 0fe0d5d

--- js/chunk/852.d19ec644.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbalm_ui_pro=self.webpackChunkbalm_ui_pro||[]).push([[852],{852:function(e,a,i){i.r(a);a.default='<h1 id="api-model">API Model</h1> <p>Defining any back-end APIs by model.</p> '}}]);
